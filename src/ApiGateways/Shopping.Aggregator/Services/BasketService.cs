c1c404440b25c815edcefbcbad029cb5929679c1
Andre Kazumi Machida <akmachid@marfrig.com.br>
2021-11-29 20:35:01 -0300

    feat: created shopping agregator api

---
+++ src/ApiGateways/Shopping.Aggregator/Services/BasketService.cs
@@ -0,0 +1,24 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<BasketModel> GetBasket(string username)
+        {
+            var response = await _httpClient.GetAsync($"/api/v1/Basket/{username}");
+            return await response.ReadContentAs<BasketModel>();
+        }
+    }
+}
